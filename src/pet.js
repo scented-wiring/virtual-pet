ce4cb2397bd650603225e1b5c18523d62d39e60d
Tom Hammersley <thomas.j.hammersley@gmail.com>
2020-07-06 12:11:46 +0100

    added checkUp function

--- src/pet.js
+++ src/pet.js
@@ -1,14 +1,42 @@
+const MAX_FITNESS = 10;
+
 function Pet(name) {
     this.name = name;
     this.age = 0;
     this.hunger = 0;
-    this.fitness = 10;
+    this.fitness = MAX_FITNESS;
 }
 
 Pet.prototype.growUp = function() {
     this.age += 1;
     this.hunger += 5;
     this.fitness -= 3;
+}
+
+Pet.prototype.walk = function () {
+    if ((this.fitness + 4) <= MAX_FITNESS) {
+        this.fitness += 4;
+    } else {
+        this.fitness = MAX_FITNESS;
+    }
+}
+
+Pet.prototype.checkUp = function () {
+    if ((this.hunger < 5) && (this.fitness > 3)) {
+        return 'I feel great!';
+    }
+
+    if ((this.hunger >= 5) && (this.fitness <= 3)) {
+        return 'I am hungry AND I need a walk.'
+    }
+
+    if (this.fitness <= 3) {
+        return 'I need a walk.';
+    }
+
+    if (this.hunger >= 5) {
+        return 'I am hungry.'
+    }
 }
 
 module.exports = Pet;
